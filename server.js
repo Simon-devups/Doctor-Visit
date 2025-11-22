5ee38b41c284843999f2b5dd39633538167c4604
Fariar <far138586@gmail.com>
2025-11-22 14:18:12 +0330

    part 2 : write models stracture

--- server.js
+++ server.js
@@ -4,115 +4,149 @@ import { fileURLToPath } from "url";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
-import { render } from "ejs";
-
+import pg from "pg"
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import connectPgSimple from "connect-pg-simple";
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT;
-
-
+const sendMassage_API = "https://hamkaransms.com/"
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './frontend')));
 app.set('views', path.join(__dirname, './frontend'));
 app.use(cors());
 
 
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+//cookie and sessions part
+const pgSession = connectPgSimple(session);
+app.use(cookieParser());
+app.use(
+    session({
+        store: new pgSession({
+            pool: pool,
+            tableName: "session"
+        }),
+        secret: "mysecret",         // sessionID
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60, // 60 minutes
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+        },
+    })
+)
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    next();
+});
+
+
 //home page
 app.get('/', (req, res) => {
-    try{
+    try {
         res.render("landing.ejs")
         // res.render("landing.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.ejs")
     }
 })
 
 //search page
 app.get('/search', (req, res) => {
-    try{
+    try {
         res.render("search.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 //login page and signup
 app.get('/login', (req, res) => {
-    try{
+    try {
         res.render("login.ejs")
-    }catch(err){
+    } catch (err) {
+        res.render("FAQ.html")
+    }
+})
+
+app.get('/code', (req, res) => {
+    try {
+        const userPhone =
+            res.render("code.ejs")
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 
+
 //choosing doctor
 app.get('/flow', (req, res) => {
-    try{
+    try {
         res.render("flow.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 
 app.get('/flow2', (req, res) => {
-    try{
+    try {
         res.render("flow2.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 
 app.get('/flow3', (req, res) => {
-    try{
+    try {
         res.render("flow3.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
-
-
-app.get('/code', (req, res) => {
-    try{
-        res.render("code.ejs")
-    }catch(err){
-        res.render("FAQ.html")
-    }
-})
-
 
 
 app.get('/questions', (req, res) => {
-    try{
+    try {
         res.render("questions.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 
 app.get('/aboutus', (req, res) => {
-    try{
+    try {
         res.render("aboutus.ejs")
-    }catch(err){
+    } catch (err) {
         res.render("FAQ.html")
     }
 })
 
 
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`running on http://localhost:${PORT}`);
 })
